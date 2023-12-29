19fbd37b30d0d604cdf39d00a96c70576ce7a280
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2023-12-29 15:22:35 +0700

    Initial commit

---
+++ lib/App/cpanm/allperls.pm
@@ -0,0 +1,17 @@
+## no critic: TestingAndDebugging::RequireUseStrict
+package App::cpanm::allperls;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+
+# ABSTRACT: Install modules for all installed perls
+
+=head1 DESCRIPTION
+
+See included script L<cpanm-allperls>.
+
+=cut
